ab8f061bebd876699bb6c4eedbc0cc759f289aa5
rajnish sharma <81792090+rajnish677@users.noreply.github.com>
2021-07-25 20:31:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+//Create variables herevar database;
+var database;
+var dog,happyDog,foodS,foodStack;
+
+
+
+
+
+function preload()
+{
+	//load images here
+  this.image = loadImage("images/dogImg.png");
+
+}
+
+function setup() {
+	createCanvas(500, 500);
+  //dog = creteSprite(250,250,20,20);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
